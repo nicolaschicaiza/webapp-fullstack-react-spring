0ae0d416592fd182d8655c2462a3f6da185ae1e4
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-10-15 15:01:54 -0500

    :test_tube: (S1C12) Relaciones de Objetos y métodos

--- sections/1-intro-js/src/main.js
+++ sections/1-intro-js/src/main.js
@@ -2,10 +2,21 @@ const invoice = {
   id: 10,
   name: "Compras de Oficina",
   date: new Date(),
-  client: "Jhon Doe",
+  client: {
+    id: 2,
+    name: 'Jhon',
+    lastname: 'Doe',
+    age: 20
+  },
   total: 1000,
+  greeting: function() {
+    return `Hello ${this.client.name}`;
+  }
 };
 
-invoice.client = "Pepe Roe";
+// invoice.client.name = 'Pepe';
 invoice.total = 5000;
 console.log(invoice);
+
+const greeting = invoice.greeting();
+console.log(greeting)
