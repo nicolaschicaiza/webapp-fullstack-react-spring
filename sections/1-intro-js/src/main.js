df19e3772c8902fa8e75acade3a675f97f2640b9
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-10-15 14:20:15 -0500

    :pencil: (S1C10) Funciones de fecha o Arraw functions

--- sections/1-intro-js/src/main.js
+++ sections/1-intro-js/src/main.js
@@ -1,9 +1,8 @@
-function sayHello(name = "Pepe", age = 0) {
-  return `Hello world fuction! ${name}, edad ${age}`;
-  // console.log("Hello world fuction!");
-}
+const sayHello = (name = "Pepe", age = 0) => `Hello world fuction! ${name}, edad ${age}`;
+const add = (a = 0, b = 0) => a + b;
 
 const result = sayHello('Andres', 18);
 
 console.log(result);
 
+console.log(add(10, 5));
