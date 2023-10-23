059383365e843ba10ef9639fe0f588d7a49aa077
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-10-23 13:47:22 -0500

    :sparkles: (S2C18) Arreglos y el operador Spread

--- sections/1-intro-js/src/main.js
+++ sections/1-intro-js/src/main.js
@@ -1,18 +1,12 @@
-const products = ['mesa', 'silla', 'notebook', 'teclado'];
+let products = ['mesa', 'silla', 'notebook', 'teclado'];
 
-products.push('pantalla lcd', 'sony tv');
+products = products.concat(['pantalla lcd', 'sony tv']);
+
+const fruits = ['peras', 'manzanas', 'sandias', 'frutillas'];
+
+const mercado = [...products, ...fruits, 'lechuga', 'papas', 'uvas'];
+const mercado2 = products.concat(fruits).concat('lechuga', 'papas', 'uvas');
 
 console.log(products);
-
-products.forEach(el => console.log(el));
-
-for (const prod of products) {
-  console.log(prod);
-}
-
-for (let i = 0; i < products.length; i++) {
-  const element = products[i];
-  console.log(element)
-}
-
-console.log(products[0])
+console.log(mercado);
+console.log(mercado2);
