9218e44cc68e3efcaa931824f0cfc85490559afa
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-10-23 15:16:45 -0500

    :sparkles: (S2C22) Arreglos el método map

--- sections/1-intro-js/src/main.js
+++ sections/1-intro-js/src/main.js
@@ -1,6 +1,85 @@
-const users = ['pepe', 'ana', 'maria', 'juan', 'sebastian', 'carlos', 'josefa']
+const invoices = [
+  {
+    id: 1,
+    name: "Compras de Oficina",
+    client: {
+      name: 'Maria',
+      lastname: 'Doe',
+    },
+    items: [
+      {
+        product: 'keyboard',
+        price: 399,
+        quantity: 2
+      },
+      {
+        product: 'mouse',
+        price: 200,
+        quantity: 1
+      },
+      {
+        product: 'paper',
+        price: 100,
+        quantity: 10
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Compras de Computación",
+    client: {
+      name: 'Pepe',
+      lastname: 'Doe',
+    },
+    items: [
+      {
+        product: 'keyboard',
+        price: 399,
+        quantity: 2
+      },
+      {
+        product: 'screen 17inch',
+        price: 800,
+        quantity: 1
+      },
+      {
+        product: 'cpu intel',
+        price: 1000,
+        quantity: 10
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: "Compras de Papeleria",
+    client: {
+      name: 'Jhon',
+      lastname: 'Doe',
+    },
+    items: [
+      {
+        product: 'pencil',
+        price: 500,
+        quantity: 1
+      },
+      {
+        product: 'paper',
+        price: 100,
+        quantity: 10
+      }
+    ]
+  },
+];
 
-// const [pepe, ana, maria, , , carlos] = users;
-const [pepe, ana, maria, ...rest] = users;
+const invoicesName = invoices.map(i => {
+  return i.name;
+});
 
-console.log(pepe, ana, maria, ...rest);
+console.log(invoices);
+console.log(invoicesName);
+
+const invoicesClient = invoices.map(i => {
+  return i.client.name;
+});
+
+console.log(invoicesClient);
