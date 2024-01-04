190a9d1d4508505e841e529e00a6abbcec8f4734
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2024-01-03 21:15:06 -0500

    :zap: (S2C31) API DOM - Document Object Model

--- sections/1-intro-js/src/main.js
+++ sections/1-intro-js/src/main.js
@@ -4,10 +4,19 @@
 
 const findAllUsers = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  return await response.json();
+  const users = await response.json();
+  const ul = document.createElement("ul");
+
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    li.innerText = user.name;
+    ul.append(li);
+    console.log(user.name);
+  });
+
+  document.getElementById("root").append(ul);
 };
 
-const users = await findAllUsers();
+findAllUsers();
 
-console.log(users);
 console.log("Hola que tal");
